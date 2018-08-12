ddb0eda8b07e0f556c02d3a234f46efd3a5fd547
hrfmmymt <hrfmmymt@gmail.com>
2018-08-12 23:40:13 +0900

    use webp

--- utils/img-opt.js
+++ utils/img-opt.js
@@ -1,23 +1,37 @@
 const Jimp = require('jimp')
 const glob = require('glob')
 const path = require('path')
+const imagemin = require('imagemin')
+const imageminWebp = require('imagemin-webp')
+
 const dist = path.join(__dirname, '../public/static/img/posts/')
 
-glob(
-  'raw/*.{jpg,JPG,jpeg,JPEG,png,PNG,gif,GIF}',
-  (err, files) => {
+async function imgOptimize() {
+  glob('raw/*.{jpg,JPG,jpeg,JPEG,png,PNG,gif,GIF}', (err, files) => {
     files.map(node => {
       Jimp.read(node)
         .then(image => {
           image
             .exifRotate()
             .resize(780, Jimp.AUTO)
-            .quality(80)
+            .quality(85)
             .write(dist + node.match('.+/(.+?)$')[1])
         })
         .catch(err => {
           console.error(err)
         })
     })
-  }
-)
+  })
+}
+
+const convertWebp = () => {
+  glob(`${dist}*.{jpg,JPG,jpeg,JPEG,png,PNG,gif,GIF}`, (err, files) => {
+    imagemin(files, `${dist}webp`, {
+      use: [imageminWebp({ quality: 85 })]
+    }).catch(err => {
+      console.error(err)
+    })
+  })
+}
+
+imgOptimize().then(() => convertWebp())
