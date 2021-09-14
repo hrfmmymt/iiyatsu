67ef3937be2aac7f8729d06f6ec762038caa6bcb
hrfmmymt <hrfmmymt@gmail.com>
2021-09-15 00:36:17 +0900

    make set recent to md files

---
+++ utils/set_recent_post_data.ts
@@ -0,0 +1,64 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { PostInfo } from './types';
+
+const postDir = path.join(__dirname, '../post/');
+const postList = JSON.parse(fs.readFileSync(path.join(__dirname, '../post-list.json'), 'utf8'));
+
+const hogehoge = 'hogehoge';
+
+const createTagOfPostEnd = ({
+  nextPost,
+  prevPost,
+}: {
+  nextPost: PostInfo['nextPost'];
+  prevPost: PostInfo['prevPost'];
+}) => {
+  const next = nextPost
+    ? `<dt>次に出た記事</dt>
+  <dd><a href="${nextPost.url}">${nextPost.title}</a></dd>`
+    : '';
+  const prev = prevPost
+    ? `<dt>前に出た記事</dt>
+  <dd><a href="${prevPost.url}">${prevPost.title}</a></dd>`
+    : '';
+  const result = `\n\n<footer>&#8718;</footer>\n<nav class="post-recent">
+  <dl>${next}${prev}</dl>\n</nav>`;
+
+  return result;
+};
+
+export const setRecentPostData = (): void => {
+  fs.readdir(postDir, (err, mdFiles) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    mdFiles.forEach((mdFile) => {
+      const thisFileIndex = postList.findIndex(
+        (post: PostInfo) => mdFile.replace(/.md/g, '') === decodeURI(post.url),
+      );
+      const nextPost: PostInfo | undefined = postList[thisFileIndex].nextPost;
+      const prevPost: PostInfo | undefined = postList[thisFileIndex].prevPost;
+      const replace = createTagOfPostEnd({ nextPost, prevPost });
+
+      fs.readFile(`${postDir}/${mdFile}`, 'utf8', (err, mdString) => {
+        if (err) {
+          return console.log(err);
+        }
+
+        if (mdString.match(replace)) {
+          return;
+        }
+
+        fs.appendFile(`${postDir}/${mdFile}`, replace, 'utf8', (err) => {
+          if (err) {
+            return console.log(err);
+          }
+        });
+      });
+    });
+  });
+};
+
+setRecentPostData();
