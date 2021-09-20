38d9a508a90805c69ce6c545bf12865908482c00
hrfmmymt <hrfmmymt@gmail.com>
2021-09-21 00:17:02 +0900

    update test of post footer

--- test/test_create_tag_of_post_end.ts
+++ test/test_create_tag_of_post_end.ts
@@ -3,13 +3,13 @@ import tap from 'tap';
 import { createTagOfPostEnd } from '../utils/create_tag_of_post_end';
 
 const EXPECT_FULL =
-  '<footer>&#8718;</footer><nav class="post-recent"><dl><dt>次に出た記事</dt><dd><a href="nextPostUrl">nextPostTitle</a></dd><dt>前に出た記事</dt><dd><a href="prevPostUrl">prevPostTitle</a></dd></dl></nav>';
+  '<footer class="post-footer">&#8718;</footer><nav class="post-recent"><dl><dt>次に出た記事</dt><dd><a href="nextPostUrl">nextPostTitle</a></dd><dt>前に出た記事</dt><dd><a href="prevPostUrl">prevPostTitle</a></dd></dl></nav>';
 
 const EXPECT_PREV_ONLY =
-  '<footer>&#8718;</footer><nav class="post-recent"><dl><dt>前に出た記事</dt><dd><a href="prevPostUrl">prevPostTitle</a></dd></dl></nav>';
+  '<footer class="post-footer">&#8718;</footer><nav class="post-recent"><dl><dt>前に出た記事</dt><dd><a href="prevPostUrl">prevPostTitle</a></dd></dl></nav>';
 
 const EXPECT_NEXT_ONLY =
-  '<footer>&#8718;</footer><nav class="post-recent"><dl><dt>次に出た記事</dt><dd><a href="nextPostUrl">nextPostTitle</a></dd></dl></nav>';
+  '<footer class="post-footer">&#8718;</footer><nav class="post-recent"><dl><dt>次に出た記事</dt><dd><a href="nextPostUrl">nextPostTitle</a></dd></dl></nav>';
 
 const INPUT_FULL = {
   nextPost: { url: 'nextPostUrl', title: 'nextPostTitle' },
