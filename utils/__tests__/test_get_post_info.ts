6f0a8f3a06671fa81e0be9d0dcc34ec56bd79215
hrfmmymt <hrfmmymt@gmail.com>
2021-09-26 00:25:15 +0900

    modified test

--- utils/__tests__/test_get_post_info.ts
+++ utils/__tests__/test_get_post_info.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import { getPostInfo } from '../get_post_info';
 
 const expectData = {
@@ -8,8 +10,10 @@ const expectData = {
   html: null,
 };
 
+const mockDir = path.join(__dirname, './mock/');
+
 test('should return data if `withHtml` is true', async () => {
-  return getPostInfo({ fileName: '20180606.md', withHtml: true }).then((data) => {
+  return getPostInfo({ postDir: mockDir, fileName: '20180606.md', withHtml: true }).then((data) => {
     expect(data.title).toBe(expectData.title);
     expect(data.description).toBe(expectData.description);
     expect(data.date).toBe(expectData.date);
@@ -19,18 +23,20 @@ test('should return data if `withHtml` is true', async () => {
 });
 
 test('should return html null if `withHtml` is false', async () => {
-  return getPostInfo({ fileName: '20180606.md', withHtml: false }).then((data) => {
-    expect(data.title).toBe(expectData.title);
-    expect(data.description).toBe(expectData.description);
-    expect(data.date).toBe(expectData.date);
-    expect(data.url).toBe(expectData.url);
-    expect(data.html).toBe(expectData.html);
-  });
+  return getPostInfo({ postDir: mockDir, fileName: '20180606.md', withHtml: false }).then(
+    (data) => {
+      expect(data.title).toBe(expectData.title);
+      expect(data.description).toBe(expectData.description);
+      expect(data.date).toBe(expectData.date);
+      expect(data.url).toBe(expectData.url);
+      expect(data.html).toBe(expectData.html);
+    },
+  );
 });
 
 test('should return an error: ENOENT', async () => {
   try {
-    await getPostInfo({ fileName: 'XXX', withHtml: false });
+    await getPostInfo({ postDir: mockDir, fileName: 'XXX', withHtml: false });
   } catch (e) {
     expect(e.code).toEqual('ENOENT');
   }
