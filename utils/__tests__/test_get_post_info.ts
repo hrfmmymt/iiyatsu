133e2c657a82acb37a765f546e9741429c1edd2b
hrfmmymt <hrfmmymt@gmail.com>
2021-09-25 10:27:15 +0900

    write test_get_post_info in jest

---
+++ utils/__tests__/test_get_post_info.ts
@@ -0,0 +1,37 @@
+import { getPostInfo } from '../get_post_info';
+
+const expectData = {
+  title: 'preact-cli でポートフォリオサイトを作った',
+  description: 'ギジュツテキ',
+  date: '2018-06-06',
+  url: '20180606',
+  html: null,
+};
+
+test('should return data if `withHtml` is true', async () => {
+  return getPostInfo({ fileName: '20180606.md', withHtml: true }).then((data) => {
+    expect(data.title).toBe(expectData.title);
+    expect(data.description).toBe(expectData.description);
+    expect(data.date).toBe(expectData.date);
+    expect(data.url).toBe(expectData.url);
+    expect(data.html).not.toBe(null);
+  });
+});
+
+test('should return html null if `withHtml` is false', async () => {
+  return getPostInfo({ fileName: '20180606.md', withHtml: false }).then((data) => {
+    expect(data.title).toBe(expectData.title);
+    expect(data.description).toBe(expectData.description);
+    expect(data.date).toBe(expectData.date);
+    expect(data.url).toBe(expectData.url);
+    expect(data.html).toBe(expectData.html);
+  });
+});
+
+test('should return an error: ENOENT', async () => {
+  try {
+    await getPostInfo({ fileName: 'XXX', withHtml: false });
+  } catch (e) {
+    expect(e.code).toEqual('ENOENT');
+  }
+});
