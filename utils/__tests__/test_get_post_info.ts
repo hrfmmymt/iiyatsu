788ec4b0394da64f1841e73bd3c673764ba0e8af
hrfmmymt <hrfmmymt@gmail.com>
2021-09-26 23:47:30 +0900

    modified test_get_post_info.ts

--- utils/__tests__/test_get_post_info.ts
+++ utils/__tests__/test_get_post_info.ts
@@ -3,35 +3,33 @@ import path from 'path';
 import { getPostInfo } from '../get_post_info';
 
 const expectData = {
-  title: 'preact-cli でポートフォリオサイトを作った',
-  description: 'ギジュツテキ',
-  date: '2018-06-06',
-  url: '20180606',
-  html: null,
+  title: 'mock-title',
+  description: 'mock-desc',
+  date: '2021-09-01',
+  url: 'mock',
+  html: 'mock-html\n',
 };
 
 const mockDir = path.join(__dirname, './mock/');
 
 test('should return data if `withHtml` is true', async () => {
-  return getPostInfo({ postDir: mockDir, fileName: '20180606.md', withHtml: true }).then((data) => {
+  return getPostInfo({ postDir: mockDir, fileName: 'mock.md', withHtml: true }).then((data) => {
     expect(data.title).toBe(expectData.title);
     expect(data.description).toBe(expectData.description);
     expect(data.date).toBe(expectData.date);
     expect(data.url).toBe(expectData.url);
-    expect(data.html).not.toBe(null);
+    expect(data.html).not.toBe(expectData.html);
   });
 });
 
 test('should return html null if `withHtml` is false', async () => {
-  return getPostInfo({ postDir: mockDir, fileName: '20180606.md', withHtml: false }).then(
-    (data) => {
-      expect(data.title).toBe(expectData.title);
-      expect(data.description).toBe(expectData.description);
-      expect(data.date).toBe(expectData.date);
-      expect(data.url).toBe(expectData.url);
-      expect(data.html).toBe(expectData.html);
-    },
-  );
+  return getPostInfo({ postDir: mockDir, fileName: 'mock.md', withHtml: false }).then((data) => {
+    expect(data.title).toBe(expectData.title);
+    expect(data.description).toBe(expectData.description);
+    expect(data.date).toBe(expectData.date);
+    expect(data.url).toBe(expectData.url);
+    expect(data.html).toBe(null);
+  });
 });
 
 test('should return an error: ENOENT', async () => {
