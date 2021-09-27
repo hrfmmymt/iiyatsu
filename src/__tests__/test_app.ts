5261ffe4b5eba2a0990a5c38dde2f9962ba86495
hrfmmymt <hrfmmymt@gmail.com>
2021-09-28 08:47:03 +0900

    add and modified test

--- src/__tests__/test_app.ts
+++ src/__tests__/test_app.ts
@@ -7,7 +7,7 @@ describe('server test', () => {
     fastify.close();
   });
 
-  test('responds with success on request /', async () => {
+  test('GET `/`: responds with success on request /', async () => {
     const response = await fastify.inject({
       method: 'GET',
       url: '/',
@@ -17,7 +17,7 @@ describe('server test', () => {
     expect(response.headers['content-type']).toBe('text/html; charset=utf-8');
   });
 
-  test('GET `/:post` post', async () => {
+  test('GET `/:post`: responds with success on request /:post', async () => {
     const response = await fastify.inject({
       method: 'GET',
       url: '/20180606',
@@ -27,7 +27,7 @@ describe('server test', () => {
     expect(response.headers['content-type']).toBe('text/html; charset=utf-8');
   });
 
-  test('GET `/api` api', async () => {
+  test('GET `/api`: responds with success on request /api', async () => {
     const response = await fastify.inject({
       method: 'GET',
       url: '/api',
@@ -37,7 +37,17 @@ describe('server test', () => {
     expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
   });
 
-  test('GET `/favicon.ico` favicon.ico', async () => {
+  test('GET `/sw.js`: responds with success on request /sw.js', async () => {
+    const response = await fastify.inject({
+      method: 'GET',
+      url: '/sw.js',
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toBe('text/javascript');
+  });
+
+  test('GET `/favicon.ico`: responds with 404 on request /favicon.ico', async () => {
     const response = await fastify.inject({
       method: 'GET',
       url: '/favicon.ico',
@@ -46,12 +56,24 @@ describe('server test', () => {
     expect(response.statusCode).toBe(404);
   });
 
-  test('GET `/:post` post', async () => {
+  test('GET `/robots.txt`: responds with success on request /robots.txt', async () => {
+    const response = await fastify.inject({
+      method: 'GET',
+      url: '/robots.txt',
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toBe('text/plain');
+    expect(response.body).toBe('User-agent: *\nDisallow: /');
+  });
+
+  test('GET `/:post`: responds with 404 on request undefined post', async () => {
     const response = await fastify.inject({
       method: 'GET',
       url: '/404',
     });
 
     expect(response.statusCode).toBe(404);
+    expect(response.headers['content-type']).toBe('text/html; charset=utf-8');
   });
 });
