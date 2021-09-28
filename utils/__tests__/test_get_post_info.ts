f60f3b8b01f23d6e2c61fead1a551a923d189461
hrfmmymt <hrfmmymt@gmail.com>
2021-09-28 11:16:39 +0900

    add and modify unit testing

--- utils/__tests__/test_get_post_info.ts
+++ utils/__tests__/test_get_post_info.ts
@@ -1,33 +1,38 @@
 import path from 'path';
 
-import { getPostInfo } from '../get_post_info';
+import { getPostInfo, highlight } from '../get_post_info';
 
-const expectData = {
-  title: 'mock-title',
-  description: 'mock-desc',
-  date: '2021-09-01',
-  url: 'mock',
-  html: 'mock-html\n',
+const EXPECT_POST_DATA = {
+  TITLE: 'mock-title',
+  DESCRIPTION: 'mock-desc',
+  DATE: '2021-09-01',
+  URL: 'mock',
+  HTML: 'mock-html\n',
+};
+
+const EXPECT_CODE_DATA = {
+  CODE: "console.log('foo');",
+  LANG: 'javascript',
 };
 
 const mockDir = path.join(__dirname, './mock/');
 
 test('should return data if `withHtml` is true', async () => {
   return getPostInfo({ postDir: mockDir, fileName: 'mock.md', withHtml: true }).then((data) => {
-    expect(data.title).toBe(expectData.title);
-    expect(data.description).toBe(expectData.description);
-    expect(data.date).toBe(expectData.date);
-    expect(data.url).toBe(expectData.url);
-    expect(data.html).not.toBe(expectData.html);
+    expect(data.title).toBe(EXPECT_POST_DATA.TITLE);
+    expect(data.description).toBe(EXPECT_POST_DATA.DESCRIPTION);
+    expect(data.date).toBe(EXPECT_POST_DATA.DATE);
+    expect(data.url).toBe(EXPECT_POST_DATA.URL);
+    expect(data.html).not.toBe(EXPECT_POST_DATA.HTML);
   });
 });
 
 test('should return html null if `withHtml` is false', async () => {
   return getPostInfo({ postDir: mockDir, fileName: 'mock.md', withHtml: false }).then((data) => {
-    expect(data.title).toBe(expectData.title);
-    expect(data.description).toBe(expectData.description);
-    expect(data.date).toBe(expectData.date);
-    expect(data.url).toBe(expectData.url);
+    expect(data.title).toBe(EXPECT_POST_DATA.TITLE);
+    expect(data.description).toBe(EXPECT_POST_DATA.DESCRIPTION);
+    expect(data.date).toBe(EXPECT_POST_DATA.DATE);
+    expect(data.url).toBe(EXPECT_POST_DATA.URL);
     expect(data.html).toBe(null);
   });
 });
@@ -38,4 +43,11 @@ test('should return an error: ENOENT', async () => {
   } catch (e) {
     expect(e.code).toEqual('ENOENT');
   }
+});
+
+test('highlight: should return highlighted code', () => {
+  expect(highlight(EXPECT_CODE_DATA.CODE, EXPECT_CODE_DATA.LANG)).toEqual(expect.anything());
+  expect(highlight(EXPECT_CODE_DATA.CODE, EXPECT_CODE_DATA.LANG)).toEqual(
+    expect.stringContaining('<span class="hljs-'),
+  );
 });
