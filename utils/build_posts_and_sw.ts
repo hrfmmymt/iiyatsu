abc26172de0f3cc4552354508deeb5d923617640
hrfmmymt <hrfmmymt@gmail.com>
2021-10-03 01:28:15 +0900

    make build_posts_and_sw

---
+++ utils/build_posts_and_sw.ts
@@ -0,0 +1,17 @@
+import * as path from 'path';
+
+import { buildSW } from './build_sw';
+import { generatePostList } from './get_post_list';
+import { setRecentPostData } from './set_recent_post_data';
+
+buildSW();
+
+async function buildPost() {
+  const postDir = path.join(__dirname, '../post/');
+  const dist = path.join(__dirname, '../');
+
+  await generatePostList({ postDir, dist });
+  setRecentPostData();
+}
+
+buildPost();
