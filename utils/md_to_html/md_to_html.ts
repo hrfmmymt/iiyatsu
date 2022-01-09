c5bb6cb6dba297201cdcbe6e826f7e8d80474aaf
hrfmmymt <hrfmmymt@gmail.com>
2022-01-09 17:24:25 +0900

    rename

--- utils/md_to_html/md_to_html.ts
+++ utils/md_to_html/md_to_html.ts
@@ -1,27 +1,19 @@
 import * as fs from 'fs';
-import { unified } from 'unified';
-import remarkParse from 'remark-parse';
-import remarkRehype from 'remark-rehype';
-import rehypeSanitize from 'rehype-sanitize';
-import rehypeStringify from 'rehype-stringify';
-import path from 'path';
+import * as path from 'path';
+import remark from 'remark';
+import html from 'remark-html';
 
 const POST_DIR = path.join(__dirname, '../../post/');
 const DIST_PATH = path.join(__dirname, '../../dist/');
 
 async function main(inputMdFile: string, fileName: string) {
-  const file = await unified()
-    .use(remarkParse)
-    .use(remarkRehype)
-    .use(rehypeSanitize)
-    .use(rehypeStringify)
-    .process(inputMdFile);
+  const file = await remark().use(html).process(inputMdFile);
 
   if (!fs.existsSync(DIST_PATH)) fs.mkdirSync(DIST_PATH);
   const outputFilePath = `${DIST_PATH}${fileName}.html`;
 
   fs.writeFile(outputFilePath, String(file), 'utf-8', (err) => {
-    err ? console.error(err) : console.log(`${outputFilePath}\n${String(file)}\n`);
+    if (err) console.error(err);
   });
 }
 
@@ -31,7 +23,7 @@ const mdToHtml = () => {
       console.log(err);
     }
 
-    for(const mdFile of mdFiles) {
+    for (const mdFile of mdFiles) {
       const filePath = POST_DIR + mdFile;
       const fileName = mdFile.replace(/.md/g, '');
       const inputMdFile = fs.readFileSync(filePath, 'utf8');
