4de06a1685d0225dde8a635003b72926dbad7ddb
hrfmmymt <hrfmmymt@gmail.com>
2022-02-19 16:40:11 +0900

    start writing

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,9 @@
 const CACHE_NAME = '::iiyatsu_serviceworker';
-const CACHE_VERSION = 16;
+const CACHE_VERSION = 17;
 const URLS_TO_CACHE = [
   "./",
   "./offline",
+  "./intake-guideline",
   "./ssr-to-ssg",
   "./20211022",
   "./20211009",
