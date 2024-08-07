d1d99ac6d45d0476cf06acd77ec8beb80cabf702
hrfmmymt <hrfmmymt@gmail.com>
2024-08-07 11:45:08 +0900

    fix: fixed?

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var constants_1 = require("./constants");
 var server = require('./src/app')({
     logger: true,
-    ignoreTrailingSlash: true
+    ignoreTrailingSlash: true,
 });
 server.listen(constants_1.CONFIG.DEV_PORT, function (err, address) {
     if (err)
         throw err;
-    console.log("server listening on " + address);
+    console.log("server listening on ".concat(address));
 });
